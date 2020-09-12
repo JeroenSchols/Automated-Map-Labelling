603f0ebb096a167c17843df1646981308d4cdb38
Chess <34745596+Chessnl@users.noreply.github.com>
2020-09-12 21:02:08 +0200

    Added documentation, refactoring untested

--- src/geoalgovis/algorithms/PullBackAlgorithm.java
+++ src/geoalgovis/algorithms/PullBackAlgorithm.java
@@ -10,39 +10,84 @@ import java.util.*;
 
 public class PullBackAlgorithm extends SymbolPlacementAlgorithm {
 
+    /*
+     * PullBack algorithm considers lines through the anchorpoint to place the symbol on
+     * It does so by finding intersections between other symbols and these lines and creating intervals over these lines
+     * In case there is a place where no interval is present, it considers placing the circle here
+     * Of all lines, we place a symbol closest to its anchor
+     */
+
     @Override
     public Output doAlgorithm(Input input) {
-        LineupAlgorithm la = new LineupAlgorithm();
-        Output output = la.doAlgorithm(input);
+        Output output = placeAway(new Output(input));
         output.symbols.sort(Comparator.comparingDouble(Circle::getRadius));
-
-        double improved;
-        int timeout = 0;
-        do {
-            timeout++;
-            improved = 0;
-            for (Symbol s : output.symbols) {
-                double dist_before = s.distanceToRegion();
-                if (dist_before == 0) continue;
-                Vector opt = findClosest(s, s.getRegion().getAnchor(), output.symbols);
-                s.getCenter().set(opt);
-                improved += dist_before - s.distanceToRegion();
-            }
-        } while (improved > 0 && timeout < 100);
-
-        if (timeout == 100) {
-            System.err.println(input.generalName() + " timed out");
-        }
-
+        output = pullBack(output, 1, true, null);
         return output;
     }
 
+    /**
+     * place all points far away from all regions to start with a clean slate
+     */
+    private Output placeAway(Output output) {
+        Rectangle boundingBox = output.getBoundingBox();
+        double x = boundingBox.width() + boundingBox.getRight();
+        double y = boundingBox.height() + boundingBox.getTop();
+        for (Symbol s : output.symbols) {
+            s.getCenter().set(x + s.getRadius(), y);
+            x += 2*s.getRadius();
+        }
+        return output;
+    }
 
-    private Vector findClosest(Symbol current, Vector goal, Collection<Symbol> obstacles) {
+    /**
+     * Apply the pullBack optimization max_iter times or until convergence is reached.
+     * Applies this to all sub-optimally placed or overlapping symbols.
+     * Try a total of 180/radi_step lines to align on.
+     *
+     * @param output any output
+     * @param max_iter the maximum number of iterations to apply pullBack, terminates earlier when converged
+     * @param is_valid whether the current output is non-overlapping, if null tests for validity
+     * @param radi_step 180/radi_step is the number of alignment-lines to consider
+     */
+    public Output pullBack(Output output, Integer max_iter, Boolean is_valid, Double radi_step) {
+        if (max_iter == null) max_iter = 1;
+        if (is_valid == null) is_valid = output.isValid();
+        if (radi_step == null) radi_step = 1d;
+
+        double improved = 1;
+        for (int iter = 0; iter < max_iter && improved > 0; iter++) {
+            improved = 0;
+            for (Symbol s : output.symbols) {
+                double dist_before = s.distanceToRegion();
+                if (dist_before == 0 && is_valid) continue; // only re-align if non-optimal or overlap is present
+                Vector opt = findClosest(s, s.getRegion().getAnchor(), output.symbols, radi_step);
+                s.getCenter().set(opt);
+                improved += dist_before - s.distanceToRegion();
+            }
+            is_valid = true;
+        }
+
+        if (improved > 0) {
+            System.out.println("pullBack did not converge on " + output.getName() + ", with max_iter = " + max_iter);
+        }
+        return output;
+    }
+
+    /**
+     * For a current symbol try and align as close as possible to the goal point without overlapping with obstacles
+     * Try a total of 180/radi_step lines to align one.
+     *
+     * @param current the current symbol to be aligned
+     * @param goal the goal to be aligned to
+     * @param obstacles all symbols (is allowed to contain current, yet filtered out)
+     * @param radi_step 180/radi_step is the number of alignment-lines to consider
+     * @return the closest point on an alignment-line for current to goal to be located on without overlapping obstacles
+     */
+    private Vector findClosest(Symbol current, Vector goal, Collection<Symbol> obstacles, double radi_step) {
         Vector dir = Vector.subtract(goal, current.getCenter());
         Vector best = current.getCenter();
 
-        for (int deg = 0; deg < 360; deg += 36) {
+        for (int deg = 0; deg < 180; deg += radi_step) {
             Vector closest = findClosest(current, goal, Vector.rotate(dir, deg), obstacles);
             if (current.getRegion().distanceToRegion(closest) < current.getRegion().distanceToRegion(best)) best = closest;
         }
@@ -50,19 +95,30 @@ public class PullBackAlgorithm extends SymbolPlacementAlgorithm {
         return best;
     }
 
+    /**
+     * For a current symbol try and align as close as possible to the goal point without
+     * overlapping with obstacles while being located on dir vector from goal.
+     *
+     * @param current the current symbol to be aligned
+     * @param goal the goal to be aligned to
+     * @param dir the direction vector of the line through the goal which current can be aligned on
+     * @param obstacles all symbols (is allowed to contain current, yet filtered out)
+     * @return the closest point on dir from goal for current to be located on without overlapping obstacles
+     */
     private Vector findClosest(Symbol current, Vector goal, Vector dir, Collection<Symbol> obstacles) {
-        Line line = new Line(goal, dir);
-        List<Event> events = new ArrayList<>();
+        Line line = new Line(goal, dir); // the line to be aligned on (line through goal with direction dir)
+        List<Event> events = new ArrayList<>(); // will store all events being starts/ends of intervals
+
         for (Symbol s : obstacles) {
-            if (s == current) continue;
-            Circle obstacle = new Circle(s.getCenter(), current.getRadius() + s.getRadius());
-            List<BaseGeometry> intersect = obstacle.intersect(line);
-            if (intersect.size() == 2) {
+            if (s == current) continue; // skip current as an obstacle
+            // get the points on the line, which when center points of current result in current and s touching
+            List<BaseGeometry> intersect = (new Circle(s.getCenter(), current.getRadius()+s.getRadius())).intersect(line);
+            if (intersect.size() == 2) { // circles for which current touches once when moving over the line are ignored
                 Vector v0 = Vector.subtract((Vector) intersect.get(0), goal);
                 Vector v1 = Vector.subtract((Vector) intersect.get(1), goal);
-                double d0 = Vector.dotProduct(v0, dir);
+                double d0 = Vector.dotProduct(v0, dir); // project these center points on the dir vector
                 double d1 = Vector.dotProduct(v1, dir);
-                if (d0 < d1) {
+                if (d0 < d1) { // let the smallest value denote the start of the interval and the largest then end
                     events.add(new Event(d0, 1));
                     events.add(new Event(d1, -1));
                 } else {
@@ -71,20 +127,21 @@ public class PullBackAlgorithm extends SymbolPlacementAlgorithm {
                 }
             }
         }
-        events.add(new Event(0, 0));
-        events.sort(Comparator.comparingDouble(event -> event.t));
+        events.add(new Event(0, 0)); // add an event to consider placing symbol on top of its anchor
+        events.sort(Comparator.comparingDouble(event -> event.t)); // sort all events on t
 
-        double best = Float.MAX_VALUE;
-        int count = 0;
+        double best = Float.MAX_VALUE; // the smallest distance considered where current can be placed from goal
+        int count = 0; // the number of currently 'active' intervals
         for (Event event : events) {
-            if (count == 0 && Math.abs(event.t) < Math.abs(best)) best = event.t;
-            count += event.delta;
-            if (count == 0 && Math.abs(event.t) < Math.abs(best)) best = event.t;
+            if (count == 0 && Math.abs(event.t) < Math.abs(best)) best = event.t; // no intervals present check event.t
+            count += event.delta; // update the number of currently 'active' intervals
+            if (count == 0 && Math.abs(event.t) < Math.abs(best)) best = event.t; // no intervals present check event.t
         }
 
-        return Vector.add(goal, Vector.multiply(best, dir));
+        return Vector.add(goal, Vector.multiply(best, dir)); // return the point closest to goal on line
     }
 
+    // class corresponding to start and end points of intervals on lines
     private class Event {
 
         double t;
