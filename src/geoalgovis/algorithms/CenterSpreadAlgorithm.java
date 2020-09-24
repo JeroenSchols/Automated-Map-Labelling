d3143350fd51df20a0fbfb1166d8fac54916f611
Chess <34745596+Chessnl@users.noreply.github.com>
2020-09-24 16:38:14 +0200

    Added a centerAreaSpread method

--- src/geoalgovis/algorithms/CenterSpreadAlgorithm.java
+++ src/geoalgovis/algorithms/CenterSpreadAlgorithm.java
@@ -8,10 +8,13 @@ import geoalgovis.symbolplacement.Symbol;
 import geoalgovis.symbolplacement.SymbolPlacementAlgorithm;
 import nl.tue.geometrycore.geometry.curved.Circle;
 import nl.tue.geometrycore.geometry.linear.HalfLine;
+import nl.tue.geometrycore.geometry.linear.Polygon;
 
 import java.util.*;
 
 public class CenterSpreadAlgorithm extends SymbolPlacementAlgorithm {
+
+    private Random random = new Random(0);
 
     /*
      * For a given point (default the average center of all symbols) place all symbols
@@ -33,7 +36,49 @@ public class CenterSpreadAlgorithm extends SymbolPlacementAlgorithm {
     }
 
     /**
+     * realigns all points multiple times around points in a central area
+     * the central area is avgAnchor + [(-delta_x, -delta_y), (delta_x, delta_y)]
+     * where delta_x = (max_x - min_x) * centersize / 2 (similar for delta_y
+     *
+     * @param symbols the symbols to be aligned
+     * @param centersize the relative size of the central area
+     * @param iter the number of iterations the central area is aligned on
+     */
+    void centerAreaSpread(List<Symbol> symbols, Double centersize, Integer iter) {
+        if (centersize == null) centersize = 0.05;
+        if (iter == null) iter = 100;
+
+        double min_x = Float.MAX_VALUE;
+        double max_x = -Float.MAX_VALUE;
+        double min_y = Float.MAX_VALUE;
+        double max_y = -Float.MAX_VALUE;
+        for (Symbol s : symbols) {
+            for (Polygon p : s.getRegion().getParts()) {
+                for (Vector v : p.vertices()) {
+                    min_x = Math.min(min_x, v.getX());
+                    max_x = Math.max(max_x, v.getX());
+                    min_y = Math.min(min_y, v.getY());
+                    max_y = Math.max(max_y, v.getY());
+                }
+            }
+        }
+
+        double delta_x = (max_x - min_x) * centersize;
+        double delta_y = (max_y - min_y) * centersize;
+
+        Vector avgAnchor = Util.getAvgAnchor(symbols);
+        centerSpread(symbols, avgAnchor);
+        for (int i = 1; i < iter; i++) {
+            Vector deltaAnchor = Vector.add(avgAnchor, new Vector(delta_x * (random.nextDouble() - 0.5), delta_y * (random.nextDouble() - 0.5)));
+            centerSpread(symbols, deltaAnchor);
+        }
+
+    }
+
+
+    /**
      * re-align symbols around the center point
+     *
      * @param symbols the symbols to be aligned
      * @param center the center point to be aligned to
      */
